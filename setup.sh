bd1994678a49ee64b84100940ed4b0252b2b8040
interwebcoding <brendon@interwebcoding.com>
2013-11-05 08:43:43 +0800

    remove dotfiles

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ sudo apt-get update
 sudo apt-get install mongodb-10gen
 
 #Installing ExpressJS
-npm install express –g
+#npm install express –g
 
 ###CONTINUE 
 #http://thecodebarbarian.wordpress.com/2013/07/22/introduction-to-the-mean-stack-part-one-setting-up-your-tools/
@@ -32,17 +32,3 @@ npm install express –g
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-
-# git pull and install dotfiles as well
-cd $HOME
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
-fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
-git clone https://github.com/interwebcoding/dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
