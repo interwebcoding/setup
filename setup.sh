d8fed8e463b22c9e86bdfe7a064d3204eb69af01
interwebcoding <brendon@interwebcoding.com>
2013-11-05 09:03:32 +0800

    redo dotfiles - fixed screen

--- setup.sh
+++ setup.sh
@@ -32,3 +32,18 @@ sudo apt-get install mongodb-10gen
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+git clone https://github.com/startup-class/dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
