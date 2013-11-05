61e20f69a498dcbace92026782f3d6ec75c867b4
interwebcoding <brendon@interwebcoding.com>
2013-11-05 12:40:39 +0800

    fix nvm issue

--- setup.sh
+++ setup.sh
@@ -9,9 +9,10 @@ sudo apt-get install -y curl
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-#nvm use v0.10.12
+#source $HOME/.nvm/nvm.sh
+#nvm install v0.10.12
+nvm install v0.10
+nvm use v0.10
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
